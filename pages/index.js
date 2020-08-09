da91b1316862098897a137b7926cfd4bd4dbef2e
Johny <janzalesinski@gmail.com>
2020-08-09 22:56:17 +0200

    next updates

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,55 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
 
-export default function Home() {
+
+export default function Home({allPostsData}) {
   return (
       <Layout home>
         <Head>
           <title>{siteTitle}</title>
         </Head>
+
         <section className={utilStyles.headingMd}>
-          <p>[Your Self Introduction]</p>
+          <p><h1>Kilka słów o autorze</h1>
+        <div className={utilStyles.info}>
+            <ul>
+                <li>Wykształcenie: wyższe inżynierskie na kierunku Informatyka - Zarządzanie projektami IT</li>
+                <li>Hobby: Programowanie [HTML,CSS,Mysql,PHP,PDO,Laravel,Next.js], Muzyka [Tworzenie aranżacji muzycznych, gra na bębnie], Excel [prowadzenie statystyk]</li>
+            </ul>
+
+        </div>
+    <p>Poniżej znajduje się lista moich wpisów</p>
+            <h2 className={utilStyles.headingLg}>Blog</h2>
+            <ul className={utilStyles.list}>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li className={utilStyles.listItem} key={id}>
+                        {title}
+                        <br />
+                        {id}
+                        <br />
+                        {date}
+                    </li>
+                ))}
+            </ul>
+
+
+          </p>
           <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+
           </p>
         </section>
+
+
       </Layout>
   )
 }
